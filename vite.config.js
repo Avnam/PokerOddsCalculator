8ad975b4e0dd93ed894aeff66eac2fceef423f79
Avnam <avnam@walla.com>
2026-06-08 02:09:22 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import obfuscator from 'vite-plugin-obfuscator';
+import { viteObfuscateFile } from 'vite-plugin-obfuscator';
 
 export default defineConfig({
   plugins: [
     react(),
-    obfuscator({
-      include: ['src/**/*.jsx', 'src/**/*.js'],
-      options: {
-        compact: true,
-        controlFlowFlattening: true,
-        controlFlowFlatteningThreshold: 0.5,
-        deadCodeInjection: true,
-        deadCodeInjectionThreshold: 0.2,
-        identifierNamesGenerator: 'hexadecimal',
-        renameGlobals: false,
-        stringArray: true,
-        stringArrayThreshold: 0.5,
-        stringArrayEncoding: ['base64'],
-        splitStrings: true,
-        splitStringsChunkLength: 5,
-        transformObjectKeys: true,
-        unicodeEscapeSequence: false,
-      },
+    viteObfuscateFile({
+      compact: true,
+      controlFlowFlattening: true,
+      controlFlowFlatteningThreshold: 0.5,
+      deadCodeInjection: true,
+      deadCodeInjectionThreshold: 0.2,
+      identifierNamesGenerator: 'hexadecimal',
+      renameGlobals: false,
+      stringArray: true,
+      stringArrayThreshold: 0.5,
+      stringArrayEncoding: ['base64'],
+      splitStrings: true,
+      splitStringsChunkLength: 5,
+      transformObjectKeys: true,
+      unicodeEscapeSequence: false,
     }),
   ],
   base: './',
